16a2efc2c77fac8a7eadbcc04b23a328bc52b2e5
Ephraim-Kamau <kephraim19@gmail.com>
2019-03-01 17:27:09 +0300

    Add the player and output panels

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-
 $(document).ready(function() {
   $('#dice').submit(function(event){
 
 var diceRoll= new String;
 var diceRoll = Math.floor(Math.random() * 6) + 1;
 
-var diceRoll = Math.floor(Math.random() * 6) + 1;
-  alert("You rolled a " + diceRoll);
+$("#scoreboard").text("You rolled a " + diceRoll);
 
 if (diceRoll === 1) {
 prompt("You have forfeited your chance");
 }
 
-});
+  });
 });
