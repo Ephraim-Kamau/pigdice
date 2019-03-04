eff0852955f2113fedfd04d5e821feb05fe46e39
Ephraim-Kamau <kephraim19@gmail.com>
2019-03-04 18:02:57 +0300

    Add changes to css

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,29 @@ var score1 = 0;
 var score2 = 0;
 var diceRoll= "";
 
-
 var diceRoll =function () { turn = Math.floor(Math.random() * 6) + 1;
 return turn;
 };
 
+function Player(name,score) {
+  this.name = name;
+  this.score = score;
+}
+Player.prototype.myChamp = function (score, name) {
+  if (score >= 100) {
+  alert("Congratulations!!" + this.name + "You have won the game." );
+}
+else {
 
+}
+};
+ var player1 = new Player(player1, score1);
+ var player2 = new Player(player2, score2);
 
 $("button#spin1").click(function(){
   var spin1 = diceRoll();
   if (spin1 != 1) {
+    player1.myChamp(score1, player1.name);
     $("#die1").text("DIE ROLL:" + spin1);
     score1 = parseInt(score1 + spin1);
     $("#cmlScore1").text("CUMMULATIVE SCORE:" + score1);
@@ -46,6 +59,7 @@ $("button#hold1").click(function() {
 $("button#spin2").click(function(){
   var spin2 = diceRoll();
   if (spin2 != 1) {
+    player1.myChamp(score2, player2.name);
     $("#die2").text("DIE ROLL:" + spin2);
     score2 = parseInt(score2 + spin2);
     $("#cmlScore2").text("CUMMULATIVE SCORE:" + score2);
